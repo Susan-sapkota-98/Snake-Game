fe54dc3817705e19c10b2e6ce9f74a17b81deee3
susan-sapkota-98 <susansapkota986@gmail.com>
2025-07-11 22:13:52 +0545

    snake game

---
+++ snakegame.py
@@ -0,0 +1,70 @@
+import turtle
+import time
+
+delay = 0.1
+
+# Set up the screen
+wn = turtle.Screen()
+wn.title("Snake Game by @Susansapkota")
+wn.bgcolor("green")
+wn.setup(width=600, height=600)
+wn.tracer(0)  # Turns off the screen update
+
+# Snake head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("black")
+head.penup()
+head.goto(0, 0)
+head.direction = "right"
+
+# Movement function
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+
+# Direction control functions
+def go_up():
+    if head.direction != "down":
+        head.direction = "up"
+
+def go_down():
+    if head.direction != "up":
+        head.direction = "down"
+
+def go_left():
+    if head.direction != "right":
+        head.direction = "left"
+
+def go_right():
+    if head.direction != "left":
+        head.direction = "right"
+
+# Keyboard bindings
+wn.listen()
+wn.onkey(go_up, "Up")
+wn.onkey(go_down, "Down")
+wn.onkey(go_left, "Left")
+wn.onkey(go_right, "Right")
+
+# Main game loop
+while True:
+    wn.update()
+    move()
+    time.sleep(delay)
+
+wn.mainloop()
